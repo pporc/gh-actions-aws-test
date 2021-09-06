19f5f26b0a638d5c886672198a47f0dfc43fd698
Syoma Jenya <syomchik007@gmail.com>
2021-09-06 15:56:30 +0300

    hello AWS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export const handler = async () => {
   const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello world'),
+        body: JSON.stringify('Hello AWS'),
     };
     return response;
 }
