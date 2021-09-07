fe277ab78142db7c7834882093a230e510610aff
Syoma Jenya <syomchik007@gmail.com>
2021-09-07 18:42:20 +0300

    fix Internal Server Error

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import Axios from "Axios";
+const Axios = require("axios").default;
 
 exports.handler = async (event) => {
   let time = new Date().getSeconds();
+  let dataString = "";
 
-  Axios.get(
-    "https://api.airtable.com/v0/appwdD9TcXOOae9pA/MainTable?sort%5B0%5D%5Bfield%5D=ID&sort%5B0%5D%5Bdirection%5D=asc",
+  await Axios.get(
+    "https://api.airtable.com/v0/TABLE_ID/MainTable?sort%5B0%5D%5Bfield%5D=ID&sort%5B0%5D%5Bdirection%5D=asc",
     {
-      headers: { Authorization: "Bearer " + "app_key" },
+      headers: { Authorization: "Bearer " + "api_key" },
     }
   )
     .then(function (response) {
       let data = response.data.records;
-      console.log(data[time % data.length].fields.title);
-
-      const res = {
-        statusCode: 200,
-        body: JSON.stringify(
-          `${data[time % data.length].fields.title}, 
-           ${data[(time + 1) % data.length].fields.title}, 
-           ${data[(time + 2) % data.length].fields.title}`
-        ),
-      };
-      return res;
+      dataString =
+        data[time % data.length].fields.title +
+        ", " +
+        data[(time + 1) % data.length].fields.title +
+        ", " +
+        data[(time + 2) % data.length].fields.title;
     })
     .catch(function (error) {
       console.log(error);
     });
+
+  const res = {
+    statusCode: 200,
+    body: JSON.stringify(dataString),
+  };
+  return res;
 };
