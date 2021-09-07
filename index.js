0b2f417891841385dcc52f34d10c645ee9aafbc1
Syoma Jenya <syomchik007@gmail.com>
2021-09-07 17:55:26 +0300

    connect airtable data

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+import Axios from "Axios";
+
 exports.handler = async (event) => {
-  const response = {
+  let time = new Date().getSeconds();
+
+  Axios.get(
+    "https://api.airtable.com/v0/appwdD9TcXOOae9pA/MainTable?sort%5B0%5D%5Bfield%5D=ID&sort%5B0%5D%5Bdirection%5D=asc",
+    {
+      headers: { Authorization: "Bearer " + "app_key" },
+    }
+  )
+    .then(function (response) {
+      let data = response.data.records;
+      console.log(data[time % data.length].fields.title);
+
+      const res = {
         statusCode: 200,
-        body: JSON.stringify('Hello AWS'),
-    };
-    return response;
-}
+        body: JSON.stringify(
+          `${data[time % data.length].fields.title}, 
+           ${data[(time + 1) % data.length].fields.title}, 
+           ${data[(time + 2) % data.length].fields.title}`
+        ),
+      };
+      return res;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
